9332bbc98f077d1dd3515c156ea4d7ba6de40a4e
DomeNTPS <tanndome_555@hotmail.com>
2021-01-19 14:46:03 +0700

    add routing

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import InspectPage from "./RiceInspect"
 
 const Login = () => {
     return (
-        <div>
-            
-        </div>
-    )
+      <div>
+        <h1>hi</h1>
+        <Link to="/InspectPage">ตรวจข้าว</Link>
+      </div>
+    );
 }
 
 export default Login
